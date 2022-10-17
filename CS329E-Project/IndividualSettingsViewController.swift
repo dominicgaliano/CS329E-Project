0d14dbf33dabacfb1835e3dfceab69929500b8b7
Dominic Galiano <dgaliano@utexas.edu>
2022-10-17 16:38:38 -0500

    added rough profile picture functionality

--- CS329E-Project/IndividualSettingsViewController.swift
+++ CS329E-Project/IndividualSettingsViewController.swift
@@ -10,14 +10,20 @@ import FirebaseAuth
 
 class IndividualSettingsViewController: UIViewController {
 
+    // Profile picture outlet
+    @IBOutlet weak var profilePicture: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profilePicture.layer.masksToBounds = true
+        profilePicture.layer.borderWidth = 2
+        profilePicture.layer.borderColor = UIColor.lightGray.cgColor
     }
     
     // Change Profile Picture Button
     @IBAction func changePictureButtonPressed(_ sender: Any) {
-        // TODO: Implement
-        return
+        presentPhotoActionSheet()
     }
     
     // Change Password Button
@@ -28,7 +34,6 @@ class IndividualSettingsViewController: UIViewController {
     
     // Dark mode slider
     @IBAction func darkModeSlider(_ sender: Any) {
-        // TODO: Implement
         return
     }
     
@@ -54,5 +59,62 @@ class IndividualSettingsViewController: UIViewController {
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
+    }
+}
+
+// User profile picture implemented here
+// TODO: Get profile picture to stay
+// TODO: Connect to user profile via firebase
+extension IndividualSettingsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func presentPhotoActionSheet() {
+        let controller = UIAlertController(
+                        title: "Profile Picture",
+                        message: "Take new profile picture or select from library",
+                        preferredStyle: .actionSheet)
+        
+        controller.addAction(UIAlertAction(title: "Cancel",
+                                           style: .cancel,
+                                           handler: nil))
+        controller.addAction(UIAlertAction(title: "Take Photo",
+                                           style: .default,
+                                           handler: { [weak self] _ in
+            self?.presentCamera()
+        }))
+        controller.addAction(UIAlertAction(title: "Choose Photo",
+                                           style: .default,
+                                           handler: { [weak self] _ in
+            self?.presentPhotoPicker()
+        }))
+        
+        present(controller, animated: true)
+    }
+    
+    func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func presentPhotoPicker() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        self.profilePicture.image = selectedImage
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
