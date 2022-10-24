b801e20012fca8cb7f342a82e817f47851ee92b9
adriancruz1 <adriancruztwosticks@gmail.com>
2022-10-23 19:10:25 -0500

    Adding Shopping List, Inventory, Group Settings.

--- CS329E-Project/InventorySubgroupViewController.swift
+++ CS329E-Project/InventorySubgroupViewController.swift
@@ -7,10 +7,23 @@
 
 import UIKit
 
-class InventorySubgroupViewController: UITableViewController {
+var Kitchen = ["Apples", "Cheese", "Chicken", "Big Pot"]
+var Restroom = ["Hand Soap", "Shampoo", "Body Wash"]
+var Bedroom = ["Light Bulbs", "Pens", "Notebook"]
 
+class InventorySubgroupViewController: UITableViewController {
+    
+    let textCellIdentifier = "TextCell"
+
+    var subgroupSelected:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -23,24 +36,101 @@ class InventorySubgroupViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        if subgroupSelected == "Kitchen" {
+            return Kitchen.count
+        }
+        else if subgroupSelected == "Restroom" {
+            return Restroom.count
+        }
+        else if subgroupSelected == "Bedroom" {
+            return Bedroom.count
+        }
+        else {
+            return 0
+        }
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath)
 
-        // Configure the cell...
+        if subgroupSelected == "Kitchen" {
+            cell.textLabel?.text = Kitchen[indexPath.row]        }
+        else if subgroupSelected == "Restroom" {
+            cell.textLabel?.text = Restroom[indexPath.row]        }
+        else if subgroupSelected == "Bedroom" {
+            cell.textLabel?.text = Bedroom[indexPath.row]        }
+        else {
+            return cell
+        }
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        /*
+        if editingStyle == .delete {
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        */
+        if subgroupSelected == "Kitchen" {
+            Kitchen.remove(at: indexPath.row)
+            if editingStyle == .delete {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+        else if subgroupSelected == "Restroom" {
+            Restroom.remove(at: indexPath.row)
+            if editingStyle == .delete {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+        else if subgroupSelected == "Bedroom" {
+            Bedroom.remove(at: indexPath.row)
+            if editingStyle == .delete {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+    }
 
+    
+    @IBAction func addButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Add New Item", message: "Add a new item to Inventory.", preferredStyle: .alert)
+        alertController.addTextField() { (textField) in
+            textField.placeholder = "Item"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [self] _ in
+            if subgroupSelected == "Kitchen" {
+                let inputName = alertController.textFields![0].text
+                Kitchen.append(inputName!)
+                tableView.insertRows(at: [IndexPath(row: Kitchen.count-1, section: 0)], with: .automatic)
+            }
+            else if subgroupSelected == "Restroom" {
+                let inputName = alertController.textFields![0].text
+                Restroom.append(inputName!)
+                tableView.insertRows(at: [IndexPath(row: Restroom.count-1, section: 0)], with: .automatic)
+            
+            }
+            else if subgroupSelected == "Bedroom" {
+                let inputName = alertController.textFields![0].text
+                Bedroom.append(inputName!)
+                tableView.insertRows(at: [IndexPath(row: Bedroom.count-1, section: 0)], with: .automatic)
+            }
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(saveAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
