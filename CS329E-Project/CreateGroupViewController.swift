7012ae72ae22ba04d32f47a8b69a149b0303d385
Dominic Galiano <dgaliano@utexas.edu>
2022-11-04 15:19:56 -0500

    Added create group partial functionality

--- CS329E-Project/CreateGroupViewController.swift
+++ CS329E-Project/CreateGroupViewController.swift
@@ -6,15 +6,21 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
 
 public var users = ["hans", "ray", "dominic", "cruz"]
 
 class CreateGroupViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     @IBOutlet weak var groupName: UITextField!
+    @IBOutlet weak var groupIdentifier: UITextField!
     @IBOutlet weak var memberName: UITextField!
     @IBOutlet weak var tableView: UITableView!
     var delegate:UIViewController!
+    
+    // establish db connection
+    let db = Firestore.firestore()
     
     //var addedMembers: [String] = []
     var addedMembers = ["hans", "ray"]
@@ -41,16 +47,21 @@ class CreateGroupViewController: UIViewController, UITableViewDelegate, UITableV
                 title: "OK", style: .default))
             present(controller, animated: true)
         }
-        else if groups.contains(groupName.text!){
-            let controller = UIAlertController(
-                title: "Error",
-                message: "Group name already in use",
-                preferredStyle: .alert)
-            controller.addAction(UIAlertAction(
-                title: "OK", style: .default))
-            present(controller, animated: true)
-        }
+//        else if groups.contains(groupName.text!){
+//            let controller = UIAlertController(
+//                title: "Error",
+//                message: "Group name already in use",
+//                preferredStyle: .alert)
+//            controller.addAction(UIAlertAction(
+//                title: "OK", style: .default))
+//            present(controller, animated: true)
+//        }
         else{
+            // create group in database
+            addGroup(groupIdentifier: groupIdentifier.text!,
+                     groupName: groupName.text!,
+                     currentUserUID: Auth.auth().currentUser!.uid)
+            
             performSegue(withIdentifier: "CreateGroupSegueBack", sender: nil)
         }
     }
@@ -89,6 +100,50 @@ class CreateGroupViewController: UIViewController, UITableViewDelegate, UITableV
             }
         }
     }
+    
+    // Adds group to database with unique group id
+    func addGroup(groupIdentifier: String, groupName: String, currentUserUID: String) {
+        // validate identifier
+        if isUnusedGroupIdentifier(groupIdentifier: groupIdentifier) {
+            // create group
+            db.collection("groups").document("groupIdentifier").setData( [
+                "groupName": groupName,
+                "users": [currentUserUID]
+            ]) {err in
+                if let err = err {
+                    print("Error adding documet: \(err)")
+                } else {
+                    print("User document added with id \(groupIdentifier)")
+                    // TODO: Add group to user's database entry
+                }
+            }
+        } else {
+            // group identifier already used, alert the user
+            let controller = UIAlertController(
+                title: "Error",
+                message: "This groupIdentifier is already in use",
+                preferredStyle: .alert)
+            controller.addAction(UIAlertAction(
+                title: "OK", style: .default))
+            present(controller, animated: true)
+        }
+    }
+    
+    // Helper function, checks db for groupIdentifier
+    func isUnusedGroupIdentifier(groupIdentifier: String) -> Bool {
+        var docRef = db.collection("groups").document(groupIdentifier)
+        
+        var isUnused: Bool = true
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                isUnused = false
+            }
+        }
+        
+        return isUnused
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         addedMembers.count
