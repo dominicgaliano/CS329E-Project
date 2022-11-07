c33ac899b4d631ff08ed33e3b027ff9ba4eef36d
Dominic Galiano <dgaliano@utexas.edu>
2022-11-06 18:49:48 -0600

    group name now appears on transition

--- CS329E-Project/GroupViewController.swift
+++ CS329E-Project/GroupViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class GroupViewController: UIViewController {
     
+    // outlets
+    @IBOutlet weak var groupNameLabel: UILabel!
+    
     var groupIdentifier:String!
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +23,27 @@ class GroupViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        print("Group identifier: \(groupIdentifier)")
+        // guard against no group selected
+        if groupIdentifier == nil {
+            print("Must select a group, cannot use shortcut anymore")
+            dismiss(animated: true)
+        }
+        
+        // get group data from database
+        let groupRef = db.collection("groups").document(groupIdentifier!)
+        
+        groupRef.getDocument { (document, error ) in
+            if let document = document, document.exists {
+                // change data where needed
+                let groupDescription = document.data()
+                print(groupDescription!["groupName"]!)
+                
+                self.groupNameLabel.text = (groupDescription!["groupName"] as! String)
+            } else {
+                print("Document does not exist")
+                self.dismiss(animated: true)
+            }
+        }
     }
     
 }
