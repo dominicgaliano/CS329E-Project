2d5bc19f09f3b9fb00709c29fd6a53a028f8bc1c
Dominic Galiano <dgaliano@utexas.edu>
2022-11-07 22:03:29 -0600

    added write capability but eliminated editing in the process,

--- CS329E-Project/ShoppingListViewController.swift
+++ CS329E-Project/ShoppingListViewController.swift
@@ -7,8 +7,7 @@
 
 import UIKit
 import FirebaseFirestore
-
-var items = ["Apples", "Toilet Paper", "Chicken Wings", "Oranges", "Tide Pods"]
+import FirebaseAuth
 
 class ShoppingListViewController: UITableViewController {
 
@@ -31,7 +30,34 @@ class ShoppingListViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        reloadTableData()
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.shoppingListItems == nil {
+            print("Table found no shopping list items")
+            return 0
+        } else {
+            print("Table found \(self.shoppingListItems.count) items(s)")
+            return self.shoppingListItems.count
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath)
+        cell.textLabel?.text = shoppingListItems![row]
+        return cell
+    }
+    
+    func reloadTableData() {
         // get group shopping list data from database
         let groupRef = db.collection("groups").document(groupIdentifier!)
         
@@ -54,52 +80,6 @@ class ShoppingListViewController: UITableViewController {
         }
     }
     
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if self.shoppingListItems == nil {
-            print("Table found no shopping list items")
-            return 0
-        } else {
-            print("Table found \(self.shoppingListItems.count) items(s)")
-            return self.shoppingListItems.count
-        }
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let row = indexPath.row
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath)
-        cell.textLabel?.text = shoppingListItems![row]
-        return cell
-    }
-    
-
-    
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-
-    
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            items.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        /*
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        */
-        }    
-    }
-    
     @IBAction func addButton(_ sender: Any) {
         let alertController = UIAlertController(title: "Add New Item", message: "Add a new item to Shopping List.", preferredStyle: .alert)
         alertController.addTextField() { (textField) in
@@ -107,38 +87,20 @@ class ShoppingListViewController: UITableViewController {
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         let saveAction = UIAlertAction(title: "Save", style: .default) { [self] _ in
-            let inputName = alertController.textFields![0].text
-            items.append(inputName!)
-            tableView.insertRows(at: [IndexPath(row: items.count-1, section: 0)], with: .automatic)
+            let newItem = alertController.textFields![0].text
+            
+            // add to database
+            let groupRef = db.collection("groups").document(self.groupIdentifier)
+            
+            groupRef.updateData([
+                "shoppingList": FieldValue.arrayUnion([newItem!])
+            ]) { _ in
+                print("Added \(String(describing: newItem!)) to group shopping list")
+                self.reloadTableData()
+            }
         }
         alertController.addAction(cancelAction)
         alertController.addAction(saveAction)
         present(alertController, animated: true, completion: nil)
     }
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
