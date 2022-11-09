c8358adc9f40bd5dc9464ff2980a99cdd117fd1f
Rayztwo <ray.zhang@utexas.edu>
2022-11-09 11:18:06 -0600

    Added a loading animation in the login screen (can be moved to other screens if necessary) and also made an Event class that can be added to the Calendar

--- CS329E-Project/ChoresSchedulerViewController.swift
+++ CS329E-Project/ChoresSchedulerViewController.swift
@@ -7,15 +7,59 @@
 
 import UIKit
 
-class ChoresSchedulerViewController: UIViewController {
+class Chore{
+    var name:String
+    var date:Date
+    var person:String
+    var replicate: Bool
+    init(name:String = "", date:Date = Date(),person:String = "", replicate: Bool = false){
+        self.name = name
+        self.date = date
+        self.person = person
+        self.replicate = replicate
+    }
+}
 
+protocol sendChore{
+    func addChore(newChore: Chore)
+}
+
+class ChoresSchedulerViewController: UIViewController {
+    
+    var delegate: sendChore? = nil
+    var newChore = Chore()
+    
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var choreLab: UITextField!
+    
+    @IBOutlet weak var personLab: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var replicateSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func choreButtonPressed(_ sender: Any) {
+        if choreLab.text == nil || personLab.text == nil{
+            statusLabel.text = "You didn't fill out everything!"
+        }else{
+            newChore.name = choreLab.text!
+            newChore.person = personLab.text!
+            newChore.date = datePicker.date
+            if replicateSwitch.isOn{
+                newChore.replicate = true
+            }else{
+                newChore.replicate = false
+            }
+        }
+        self.delegate?.addChore(newChore: newChore)
+        print("hi")
+    }
+    
     /*
     // MARK: - Navigation
 
--- CS329E-Project/ChoresViewController.swift
+++ CS329E-Project/ChoresViewController.swift
@@ -6,28 +6,53 @@
 //
 
 import UIKit
+var choreList: [Chore] = []
 
-class ChoresViewController: UIViewController{
+class ChoresViewController: UIViewController, UICalendarSelectionSingleDateDelegate, sendChore{
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        print(choreList)
+        return 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         createCalendar()
     }
     func createCalendar(){
+        //create calendar
         let calendarView = UICalendarView()
         calendarView.translatesAutoresizingMaskIntoConstraints = false
         
+        //calendar settings
         calendarView.calendar = .current
         calendarView.locale = .current
         calendarView.fontDesign = .rounded
         calendarView.delegate = self
         
+        //make the selection single selection
+        let dateSelection = UICalendarSelectionSingleDate(delegate: self)
+        calendarView.selectionBehavior = dateSelection
+        
         view.addSubview(calendarView)
+
     }
-    
+    func addChore(newChore: Chore) {
+        choreList.append(newChore)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "choreSchedSegue" {
+            let secondVC: ChoresSchedulerViewController = segue.destination as! ChoresSchedulerViewController
+            secondVC.delegate = self
+        }
+    }
+
+
 }
 
 extension ChoresViewController: UICalendarViewDelegate{
     func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
-        return nil
+        let font = UIFont.systemFont(ofSize: 10)
+        let configuration = UIImage.SymbolConfiguration(font: font)
+        let image = UIImage(systemName: "circle.fill", withConfiguration: configuration)?.withRenderingMode(.alwaysOriginal)
+        return .image(image)
     }
 }
